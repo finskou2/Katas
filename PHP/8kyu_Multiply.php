9513fc2a8ec7d42712e05488996b5c0d2de718d1
Finskou <lelarhaz@gmail.com>
2020-05-08 18:26:05 +0200

    Multiply Kata

---
+++ PHP/8kyu_Multiply.php
@@ -0,0 +1,14 @@
+// The code does not execute properly. Try to figure out why.
+// 
+//  function multiply(a, b){
+//  return a * b
+//  }
+
+<?php 
+
+function multiply($a, $b) {
+  $sum = $a * $b;
+  return $sum;
+}
+
+?>
